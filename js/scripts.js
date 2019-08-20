29c67ae0fa249bc6ad454cf0c54992820de0037c
Carrie Schmid and Loewy Malkovich <loewy@>
2019-08-20 16:21:27 -0700

    add for loops to identify which elements to replace

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
-var vowels = ["a", "e", "i", "o", "u"];
+// business logic
 
+
+
+
+// front end ui
 $(document).ready(function(){
+  var vowels = ["a", "e", "i", "o", "u"];
   $("#stringInput").submit(function(event){
     event.preventDefault();
-    var letters = $("#sentenceInput").val().split('');
-    console.log(letters);
-    letters.forEach(function(letter){
-
-      if (letter === "a" || "e" || "i"||"o"||"u") {
-        return letter = "-";
-      } else {
-        return letter;
-      };
-
-    });
-
-  $("#output").text(letters);
-
+    var letters = $("#sentenceInput").val();
+    var outputString = "";
+      for (var i=0; i<letters.length; i++) {
+        for (var x=0; x<vowels.length; x++) {
+          if (letters[i] === vowels[x]) {
+            outputString += "-";
+            console.log(letters);
+          } else {
+            outputString += letters[i];
+            break;
+          }
+        }
+      }
+      $("#output").text(outputString);
   });
 });
